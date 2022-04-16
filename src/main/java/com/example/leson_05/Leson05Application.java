9e02dbbd1ce1cd49f9b93b6363f2cdd1d785f69c
Jo'raqulov O'tkirjon <97774479+FranchiscoMike@users.noreply.github.com>
2022-04-16 13:36:43 +0500

    Initial commit

---
+++ src/main/java/com/example/leson_05/Leson05Application.java
@@ -0,0 +1,13 @@
+package com.example.leson_05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Leson05Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Leson05Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/leson_05/Leson05ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.leson_05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Leson05ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
